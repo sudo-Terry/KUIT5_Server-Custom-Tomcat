f4ef2a82de7079407e82308aa86434cb6563f001
sudo-Terry <contact.terrykim@gmail.com>
2025-04-01 17:50:56 +0900

    feat: 요청 경로에 알맞은 정적 파일 제공 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,12 +2,14 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class RequestHandler implements Runnable{
     Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
+    private static final String WEB_DOC_ROOT = "webapp";
 
     public RequestHandler(Socket connection) {
         this.connection = connection;
@@ -20,13 +22,47 @@ public class RequestHandler implements Runnable{
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            String requestPath = parseRequest(br);
+            if (requestPath == null) {
+                throw new IOException("Invalid request path");
+            }
+
+            File file = matchFileByRequestPath(requestPath);
+            if (file == null) {
+                response404Header(dos);
+            } else {
+                byte[] body = Files.readAllBytes(file.toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
 
         } catch (IOException e) {
             log.log(Level.SEVERE,e.getMessage());
         }
+    }
+
+    // HTTP 요청에서 경로 추출
+    private String parseRequest(BufferedReader br) throws IOException {
+        String requestLine = br.readLine();
+        if (requestLine == null || requestLine.isEmpty()) {
+            return null;
+        }
+        log.log(Level.INFO, "Request Line: " + requestLine);
+
+        // 경로 추출 "GET /index.html HTTP/1.1"
+        String[] tokens = requestLine.split(" ");
+
+        return tokens[1];
+    }
+
+    // 전달받은 path에 알맞은 파일을 서버에서 찾아 반환
+    private File matchFileByRequestPath(String requestPath) throws IOException {
+        if (requestPath.equals("/")) requestPath = "/index.html";
+
+        File file = new File(WEB_DOC_ROOT + requestPath);
+        if (file.exists()) return file;
+
+        return null;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
@@ -35,6 +71,22 @@ public class RequestHandler implements Runnable{
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void response404Header(DataOutputStream dos) {
+        try {
+            String errorMessage = "<h1>404 Not Found</h1>";
+            byte[] body = errorMessage.getBytes();
+
+            dos.writeBytes("HTTP/1.1 404 Not Found\r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + body.length + "\r\n");
+            dos.writeBytes("\r\n");
+            dos.write(body, 0, body.length);
+            dos.flush();
         } catch (IOException e) {
             log.log(Level.SEVERE, e.getMessage());
         }
